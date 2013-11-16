29c4a771e415f3e30aac39254fa2f022061b7cc5
Chris Snyder <csnyder@unleashed-technologies.com>
2013-11-16 18:54:58 -0500

    Add DB library and set up config

--- load_libraries.php
+++ load_libraries.php
@@ -1,2 +1,17 @@
 <?php
+//load the template needed
 require_once ('template/template.php');
+require_once('libraries/OBJ-MySQL/bin/OBJ_mysql.php');
+
+//setup db
+
+    $config = array();
+    $config["hostname"]  = "localhost ";
+    $config["database"]  = "dagrdb";
+    $config["username"]  = "dagrdb";
+    $config["password"]  = "snyder";
+
+    //class instantiation
+    $db = new OBJ_mysql($config);
+
+
