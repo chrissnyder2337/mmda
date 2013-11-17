b0df48a7acf6a390d88405bb39a852b12d18a47d
Chris Snyder <csnyder@unleashed-technologies.com>
2013-11-16 23:22:43 -0500

    Create Tika Wrapper

--- libraries/tikawrapper.php
+++ libraries/tikawrapper.php
@@ -1,9 +1,73 @@
 <?php
 
+//use Symfony\Component\Process\Process;
 
-class TikaWrapper{
+class TikaWrapper
+{
+    private function execute($option, $filepath)
+    {
+
+        $dir = __DIR__.'/';
+        $command = 'java -jar '.$dir.'tika-app-1.4.jar '.$option.' '.$filepath;
+
+        //$process = new Process($command);
+        //$process->setWorkingDirectory($cwd);
+
+        exec($command,$output,$return_var);
+        //$process->run();
+        print "<pre>";
+        print_r($command);
+        print_r($dir);
+        print_r($output);
+
+        $output = implode("\n", $output);
+
+        print_r($output);
+        print "</pre>";
+        if (empty($output)) {
+            throw new \RuntimeException($output);
+        }
+
+        return $output;
+
+    }
+
+    public function getText($filepath)
+    {
+        return $this->execute("--text", $filepath);
+    }
+
+    public function getTextMain($filepath)
+    {
+        return $this->execute("--text-main", $filepath);
+    }
 
 
+    public function getLanguage($filepath)
+    {
+        return $this->execute("--language", $filepath);
+    }
+
+    public function getContentType($filepath)
+    {
+        return $this->execute("--detect", $filepath);
+    }
+
+    public function getXhtml($filepath)
+    {
+        return $this->execute("--xml", $filepath);
+
+    }
+    public function getHtml($filepath)
+    {
+        return $this->execute("--html", $filepath);
+    }
+
+    public function getMetaData($filepath)
+    {
+        $jsonMeta = $this->execute("--json", $filepath);
+        return json_decode($jsonMeta);
+    }
 
 
 }
--- mmda.php
+++ mmda.php
@@ -3,7 +3,52 @@
 /**
  * Givin a file will add it to db and return results.
  */
-function addFile($file){
+function mmda_add_file($filepath){
+
+  //get hash of file
+  $file_hash = md5_file($filepath);
+  $filename = basename($filepath);
+
+  //check if file already exists based on hash / name
+  if(!mmda_check_file_exists($filename,$file_hash)){
+
+    $metadata =  mmda_get_metadata($filepath);
+
+    print "<pre>";
+    print_r($metadata);
+    print "</pre>";
+  }else{
+    return "File already in db";
+  }
+
+
+
+  //TODO grab metadata from tika which will give back an array of data
+
+  //
 
   return 'query results';
+}
+
+function mmda_check_file_exists($filename,$filehash)
+{
+  global $db;
+  $query = $db->query("SELECT count(*) as count FROM File WHERE md5_hash = ? and resource_name = ?",array($filehash,$filename));
+  $result = $query->fetchAll();
+
+  if($result[0]->count > 0){
+    return TRUE;
+  }
+
+  return false;
+}
+
+function mmda_get_metadata($filepath){
+
+
+  $tika = new TikaWrapper();
+  return $tika->getMetaData($filepath);
+
+
+
 }
