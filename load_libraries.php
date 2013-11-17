ecfeeedaf4428da3b829a1f7eded9c2b2780c4b1
Chris Snyder <csnyder@unleashed-technologies.com>
2013-11-16 19:03:30 -0500

    Load the template library

--- load_libraries.php
+++ load_libraries.php
@@ -3,15 +3,22 @@
 require_once ('template/template.php');
 require_once('libraries/OBJ-MySQL/bin/OBJ_mysql.php');
 
-//setup db
+//SET UP DB
 
-    $config = array();
-    $config["hostname"]  = "localhost ";
-    $config["database"]  = "dagrdb";
-    $config["username"]  = "dagrdb";
-    $config["password"]  = "snyder";
+$config = array();
+$config["hostname"]  = "localhost ";
+$config["database"]  = "dagrdb";
+$config["username"]  = "dagrdb";
+$config["password"]  = "snyder";
 
-    //class instantiation
-    $db = new OBJ_mysql($config);
+//class instantiation
+$db = new OBJ_mysql($config);
+
+
+// SET Template
+$template = new MMDA_Template();
+
+
+
 
 
