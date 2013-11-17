bd13e3affc1567cac07c1525dfa743b38e8c20a2
Chris Snyder <csnyder@unleashed-technologies.com>
2013-11-16 23:38:14 -0500

    Initial commit of metadata_attributes

---
+++ metadata_attributes.php
@@ -0,0 +1,78 @@
+<?php
+
+
+$metadata_attributes = array(
+  'resource_name' => array(
+    'table' => 'File',
+    'tika_alias' => array('resourceName')
+    ),
+  'local_path' => array(
+    'table' => 'File',
+    'tika_alias' => array()
+    ),
+  'external_path' => array(
+    'table' => 'File',
+    'tika_alias' => array()
+    ),
+  'content_type' => array(
+    'table' => 'File',
+    'tika_alias' => array('Content-Type')
+    ),
+  'content_length' => array(
+    'table' => 'File',
+    'tika_alias' => array('Content-Length')
+    ),
+  'md5_hash' => array(
+    'table' => 'File',
+    'tika_alias' => array()
+    ),
+  'audio_bitrate' => array(
+    'table' => 'AudioMetadata',
+    'tika_alias' => array()
+    ),
+  'audio_samplerate' => array(
+    'table' => 'AudioMetadata',
+    'tika_alias' => array()
+    ),
+  'channels' => array(
+    'table' => 'AudioMetadata',
+    'tika_alias' => array()
+    ),
+  'channel_type' => array(
+    'table' => 'AudioMetadata',
+    'tika_alias' => array()
+    ),
+  'audio_format' => array(
+    'table' => 'AudioMetadata',
+    'tika_alias' => array()
+    ),
+  'track_number' => array(
+    'table' => 'AudioMetadata',
+    'tika_alias' => array()
+    ),
+  'title' => array(
+    'table' => 'AudioMetadata',
+    'tika_alias' => array()
+    ),
+  'genre' => array(
+    'table' => 'AudioMetadata',
+    'tika_alias' => array()
+    ),
+  'duration' => array(
+    'table' => 'AudioMetadata',
+    'tika_alias' => array()
+    ),
+  'artist' => array(
+    'table' => 'AudioMetadata',
+    'tika_alias' => array()
+    ),
+  'album' => array(
+    'table' => 'AudioMetadata',
+    'tika_alias' => array()
+    ),
+  'audio_compression' => array(
+    'table' => 'AudioMetadata',
+    'tika_alias' => array()
+    ),
+
+  );
