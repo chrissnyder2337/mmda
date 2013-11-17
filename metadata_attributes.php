6d28f002603d0a2390b28ff3950ba62b6d0ae139
jpsnyder <jonsnyder1@gmail.com>
2013-11-17 14:12:07 -0500

    Added more metadata attributes to array

--- metadata_attributes.php
+++ metadata_attributes.php
@@ -116,12 +116,125 @@ $metadata_attributes = array(
     'tika_alias' => array('Creation-Date','meta:creation-date','dcterms:created','Date/Time Original')
     ),
   'last_modified_date' => array(
-'filterable' => TRUE,
+    'filterable' => TRUE,
     'display' => 'Last Modified Date',
     'table' => 'AuthoringMetadata',
     'tika_alias' => array('modified','dcterms:modified','date','Last-Modified')
     ),
-
+  'image_count' => array(
+    'filterable' => TRUE,
+    'display' => 'Image Count',
+    'table' => 'DocumentCountsMetadata',
+    'tika_alias' => array()
+    ),
+  'page_count' => array(
+    'filterable' => TRUE,
+    'display' => 'Page Count',
+    'table' => 'DocumentCountsMetadata',
+    'tika_alias' => array()
+    ),  
+  'table_count' => array(
+    'filterable' => TRUE,
+    'display' => 'Table Count',
+    'table' => 'DocumentCountsMetadata',
+    'tika_alias' => array()
+    ),  
+  'paragraph_count' => array(
+    'filterable' => TRUE,
+    'display' => 'Paragraph Count',
+    'table' => 'DocumentCountsMetadata',
+    'tika_alias' => array()
+    ),
+  'character_count' => array(
+    'filterable' => TRUE,
+    'display' => 'Character Count (without spaces)',
+    'table' => 'DocumentCountsMetadata',
+    'tika_alias' => array()
+    ),
+  'word_count' => array(
+    'filterable' => TRUE,
+    'display' => 'Word Count',
+    'table' => 'DocumentCountsMetadata',
+    'tika_alias' => array()
+    ),
+  'character_count_with_space' => array(
+    'filterable' => TRUE,
+    'display' => 'Character Count',
+    'table' => 'DocumentCountsMetadata',
+    'tika_alias' => array()
+    ),
+  'slide_count' => array(
+    'filterable' => TRUE,
+    'display' => 'Slide Count',
+    'table' => 'DocumentCountsMetadata',
+    'tika_alias' => array()
+    ),
+  'architecture_bits' => array(
+    'filterable' => TRUE,
+    'display' => 'Architecture Bits',
+    'table' => 'ExecutableMetadata',
+    'tika_alias' => array()
+    ),
+  'machine_type' => array(
+    'filterable' => TRUE,
+    'display' => 'Machine Type',
+    'table' => 'ExecutableMetadata',
+    'tika_alias' => array()
+    ),
+  'machine_platform' => array(
+    'filterable' => TRUE,
+    'display' => 'Machine Platform',
+    'table' => 'ExecutableMetadata',
+    'tika_alias' => array()
+    ),
+  'x_resolution' => array(
+    'filterable' => TRUE,
+    'display' => 'X Axis Resolution',
+    'table' => 'ImageResolutionMetadata',
+    'tika_alias' => array()
+    ),
+  'y_resoltuion' => array(
+    'filterable' => TRUE,
+    'display' => 'Y Axis Resolution',
+    'table' => 'ImageResolutionMetadata',
+    'tika_alias' => array()
+    ),
+  'resolution_units' => array(
+    'filterable' => TRUE,
+    'display' => 'Resolution Units',
+    'table' => 'ImageResolutionMetadata',
+    'tika_alias' => array()
+    ),
+  'video_datarate' => array(
+    'filterable' => TRUE,
+    'display' => 'Video Datarate',
+    'table' => 'VideoMetadata',
+    'tika_alias' => array()
+    ),
+  'video_format' => array(
+    'filterable' => TRUE,
+    'display' => 'Video Format',
+    'table' => 'VideoMetadata',
+    'tika_alias' => array()
+    ),
+  'video_duration' => array(
+    'filterable' => TRUE,
+    'display' => 'Video Duration',
+    'table' => 'VideoMetadata',
+    'tika_alias' => array()
+    ),
+  'audio_duration' => array(
+    'filterable' => TRUE,
+    'display' => 'Audio Duration',
+    'table' => 'VideoMetadata',
+    'tika_alias' => array()
+    ),
+  'webpage_title' => array(
+    'filterable' => TRUE,
+    'display' => 'Webpage Title',
+    'table' => 'WebpageMetadata',
+    'tika_alias' => array()
+    )
   );
 
 
